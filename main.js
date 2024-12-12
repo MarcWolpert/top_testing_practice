3e1c848e61f377cdc1c1459b7e99177de0d3d52d
Marc Wolpert <mewolpert@gmail.com>
2024-12-11 23:45:28 -0800

    test:Tests except caesar cipher work

--- main.js
+++ main.js
@@ -1,4 +1,51 @@
-module.exports = { capitalize };
-function capitalize(input) {
-	return input.toLocalUpperCase('en-US');
+function capitalize(input, locale) {
+	return input.toLocaleUpperCase(locale);
 }
+
+function reverseString(input) {
+	return input.split('').reverse().join('');
+}
+
+const Calculator = {
+	add(a, b) {
+		return a + b;
+	},
+	subtract(a, b) {
+		return a - b;
+	},
+	divide(a, b) {
+		return a / b;
+	},
+	multiply(a, b) {
+		return a * b;
+	},
+};
+
+function caesarCipher(input, shift) {
+	//65->90 for uppercase
+	let uppercaseModulo = 95 - 65;
+	//97->122 for lowercase
+	let lowercaseModulo = 122 - 97;
+	let cipher = '';
+	for (char of input) {
+		let charCode = char.charCodeAt(0);
+		if (charCode > 122) {
+			continue;
+		}
+		if (charCode === 96) {
+			continue;
+		}
+		if (charCode < 65) {
+			continue;
+		}
+		if (charCode < 96) {
+			charCode = ((charCode - 65 + shift) % uppercaseModulo) + 65;
+		} else if (charCode > 96) {
+			charCode = ((charCode - 97 + shift) % lowercaseModulo) + 97;
+		}
+		cipher = cipher + String.fromCharCode(charCode);
+	}
+	return cipher;
+}
+
+module.exports = { capitalize, reverseString, Calculator, caesarCipher };
--- main.test.js
+++ main.test.js
@@ -5,9 +5,49 @@ test('works', () => {
 });
 
 test('Capitalize Marc', () => {
-	expect(main.capitalize('marc')).toBe('MARC');
+	expect(main.capitalize('marc', 'en-US')).toBe('MARC');
 });
 
 test('Capitalize Sally', () => {
-	expect(main.capitalize('sally')).toBe('SALLY');
+	expect(main.capitalize('sally', 'en-US')).toBe('SALLY');
+});
+
+test('Capitalize Adamantia', () => {
+	expect(main.capitalize('Αδαμαντία', 'el-GR')).toBe('ΑΔΑΜΑΝΤΙΑ');
+});
+
+test('String function recognized', () => {
+	expect(main.reverseString('asd')).toBe('dsa');
+});
+
+test('String function reverses more than one', () => {
+	expect(main.reverseString('murder')).toBe('redrum');
+});
+
+test('String function reverses other language', () => {
+	expect(main.reverseString('Αδαμαντία')).toBe('αίτναμαδΑ');
+});
+
+test('calculator add works', () => {
+	expect(main.Calculator.add(2, 2)).toBe(4);
+});
+
+test('calculator subtract works', () => {
+	expect(main.Calculator.subtract(4, 2)).toBe(2);
+});
+
+test('calculator divide works', () => {
+	expect(main.Calculator.divide(8, 2)).toBe(4);
+});
+
+test('calculator multiply works', () => {
+	expect(main.Calculator.multiply(8, 2)).toBe(16);
+});
+
+test('caesar cipher exists', () => {
+	expect(main.caesarCipher('xyz', 3)).toBe('abc');
+});
+
+test('caesar cipher works on lowercase', () => {
+	expect(main.caesarCipher('abc', 5)).toBe('fgh');
 });
